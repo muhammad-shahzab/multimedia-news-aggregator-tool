05bcfcb92956c7d18eec225ddaf3fda8fe3899df
MUHAMMAD SHAHZAIB <mshahzaib0003@gmail.com>
2025-07-04 00:41:57 +0500

    Project Initalization

--- server/server.js
+++ server/server.js
@@ -9,19 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Import routes
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/users');
-
-// API routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-
-// Connect MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
